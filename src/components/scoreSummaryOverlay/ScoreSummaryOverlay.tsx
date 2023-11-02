a7ed28f7b0df753b97c165f1600925fbda0b28f0
Thiti Chainiyom <97273062+tokecny@users.noreply.github.com>
2023-11-02 17:02:14 +0700

    revise ScoreSummaryOverlay
    - add dprime and scorePercentile as props
    - add userAge

--- src/components/scoreSummaryOverlay/ScoreSummaryOverlay.tsx
+++ src/components/scoreSummaryOverlay/ScoreSummaryOverlay.tsx
@@ -7,12 +7,14 @@ function ScoreSummaryOverlay(props) {
     <div className="summary">
       <div className="summaryContainer">
           <div className="summaryHeader">
-              <p className='thisScore'>คะแนนครั้งนี้</p>
-              <p className='bigScore'>{props.sumScores.toLocaleString()}</p>
+              <p className='thisScore'>คะแนนครั้งนี้ : {props.sumScores.toLocaleString()}</p>
+              <p className='thisScore'>d' : {(props.dprime).toFixed(1)} ({props.scorePercentile} %)</p>
+              {/* <p className='bigScore'>{props.sumScores.toLocaleString()}</p> */}
+
           </div>
           <div className="summaryBadge">
               <img src={scoreBadge} alt='badge' className={'medalBadge'}></img>
-              {/* <p className='highestScore'>คะแนนสูงสุด: 67,445</p> */}
+              <p className='highestScore'>อายุ : <b>{props.userAge}</b> ปี</p>
           </div>
           <div className="btnContainerSummary">
               <button className='summaryBtnPlayAgain' onClick={props.refreshPage}>{`เล่นอีกครั้ง`}</button>
